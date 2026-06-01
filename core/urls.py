ff445dbcc5bde70164d978a06423daa6a17eb92c
Mityana Pentecostal Church <mityanapentostalchurch@gmail.com>
2026-06-01 17:08:02 +0300

    add core pages

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, about, leadership, contact
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('leadership/', leadership, name='leadership'),
+    path('contact/', contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def leadership(request):
+    return render(request, 'leadership.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
